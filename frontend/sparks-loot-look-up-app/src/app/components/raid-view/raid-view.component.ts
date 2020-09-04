f94a5f2c54862684f416caef20735930da7922dc
Brust <pbrust90@gmail.com>
2020-09-04 22:16:42 +0200

    feat: fixed raider view

--- frontend/sparks-loot-look-up-app/src/app/components/raid-view/raid-view.component.ts
+++ frontend/sparks-loot-look-up-app/src/app/components/raid-view/raid-view.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {BasicType} from '../../dataTypes/shared-data-types';
+import {BasicType, LootListData} from '../../dataTypes/shared-data-types';
 import {RequestService} from '../../services/request.service';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
@@ -22,71 +22,65 @@ export class RaidViewComponent implements OnInit {
   filteredRaids: Observable<BasicType[]>;
 
   items: BasicType[];
+  dataSource: BasicType[];
+  displayedColumns: string[] = ['item_name', 'rname', 'prioritynb'];
 
   constructor(private requestService: RequestService) {
   }
 
   ngOnInit(): void {
-    this.getItems();
-    this.getRaidOptions();
-    this.getBossOptions();
+    this.setUp();
   }
 
-  private getItems(): void {
-    // separate because of async calls
+  private setUp(): void {
     this.requestService.getItemList().subscribe(data => {
-      this.items = data;
+      this.items = [...data];
+      this.getRaidOptions(data);
+      this.getBossOptions(data);
     });
   }
 
-  private getBossOptions(): void {
-    this.requestService.getItemList().subscribe(data => {
-      this.bossesFull = [];
-      for (const entry of data) {
-        let exists = false;
-        for (const item of this.bossesFull) {
-          if (entry.boss === item.boss) {
-            exists = true;
-          }
-        }
-        if (!exists) {
-          this.bossesFull.push(entry);
+  private getBossOptions(data: BasicType[]): void {
+    this.bossesFull = [];
+    for (const entry of data) {
+      let exists = false;
+      for (const item of this.bossesFull) {
+        if (entry.boss === item.boss) {
+          exists = true;
         }
       }
-
-      console.log(this.bossesFull);
-      this.bossesReduced = [...this.bossesFull];
-      this.filteredBosses = this.selectedBoss.valueChanges.pipe(
-        startWith(''),
-        map(value => this._BossFilter(value))
-      );
-    });
+      if (!exists) {
+        this.bossesFull.push(entry);
+      }
+    }
+    this.bossesReduced = [...this.bossesFull];
+    this.filteredBosses = this.selectedBoss.valueChanges.pipe(
+      startWith(''),
+      map(value => this._BossFilter(value))
+    );
   }
 
-  private getRaidOptions(): void {
-    this.requestService.getItemList().subscribe(data => {
-      this.raids = [];
-      for (const entry of data) {
-        let exists = false;
-        for (const item of this.raids) {
-          if (entry.raid === item.raid) {
-            exists = true;
-          }
-        }
-        if (!exists) {
-          this.raids.push(entry);
+  private getRaidOptions(data: BasicType[]): void {
+    this.raids = [];
+    for (const entry of data) {
+      let exists = false;
+      for (const item of this.raids) {
+        if (entry.raid === item.raid) {
+          exists = true;
         }
       }
-      this.filteredRaids = this.selectedRaid.valueChanges.pipe(
-        startWith(''),
-        map(value => this._RaidFilter(value))
-      );
-    });
+      if (!exists) {
+        this.raids.push(entry);
+      }
+    }
+    this.filteredRaids = this.selectedRaid.valueChanges.pipe(
+      startWith(''),
+      map(value => this._RaidFilter(value))
+    );
   }
 
   private _RaidFilter(value: string): BasicType[] {
     const filterValue = value.toLowerCase();
-    console.log(this.bossesReduced);
     return this.raids.filter(option => option.raid.toLowerCase().includes(filterValue));
   }
 
@@ -96,10 +90,14 @@ export class RaidViewComponent implements OnInit {
   }
 
   bossChanged(): void {
-
+    this.dataSource = this.items.filter(option => option.boss === this.selectedBoss.value);
   }
 
   raidChanged(): void {
     this.bossesReduced = this.bossesFull.filter(data => data.raid === this.selectedRaid.value);
+    this.selectedBoss.setValue('');
+    this.dataSource = this.items.filter(option => option.raid === this.selectedRaid.value);
+    this.dataSource.sort((n1, n2) => n1.prioritynb - n2.prioritynb);
+    this.dataSource.sort((n1, n2) => n2.item_name.localeCompare(n1.item_name));
   }
 }
